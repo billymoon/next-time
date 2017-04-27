0e501c199a4f906c2ea0b2e378668c7ecd1448ec
Billy Moon <billy@itaccess.org>
2017-04-27 22:37:35 +0100

    fix shadow, correct timing, and allow add to home screen

--- pages/index.js
+++ pages/index.js
@@ -1,101 +1,97 @@
 import React from 'react'
+import Head from 'next/head'
 
 const Hour = props =>
-  <svg className='hour' version='1.1' xmlns='http://www.w3.org/2000/svg' x='0' y='0' width='100' height='86.6' viewBox='0, 0, 100, 86.6'>
-    <filter id='blur-filter' x='-2' y='-2' width='200' height='200'>
-      <feGaussianBlur in='SourceGraphic' stdDeviation='2' />
-    </filter>
+  <svg className='hour' version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0, 0, 120, 103.924'>
     <g>
-      <path d='M-0,86.603 L25,43.302 L50,0 L75,43.301 L100,86.603 L50,86.603 z' fill='#000000' />
+      <path d='M7.125,97.753 L33.563,51.962 L60,6.17 L86.438,51.962 L112.876,97.753 L60,97.753 z' fill='#000000' />
     </g>
   </svg>
 
 const Minute = props =>
-  <svg className='minute' version='1.1' xmlns='http://www.w3.org/2000/svg' x='0' y='0' width='100' height='86.6' viewBox='0, 0, 100, 86.6'>
-    <filter id='blur-filter' x='-2' y='-2' width='200' height='200'>
-      <feGaussianBlur in='SourceGraphic' stdDeviation='2' />
-    </filter>
+  <svg className='minute' version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0, 0, 120, 103.924'>
     <g>
-      <path d='M50,0 L75,43.301 L100,86.603 L-0,86.603 L25,43.302 L50,0 z M46.721,16.897 L9.999,80.5 L83.442,80.5 L46.721,16.897 z' fill='#ffffff' />
+      <path d='M60,6.17 L86.438,51.962 L112.876,97.753 L7.125,97.753 L33.563,51.962 L60,6.17 z M56.532,24.039 L17.7,91.299 L95.365,91.299 L56.532,24.039 z' fill='#fff' />
     </g>
   </svg>
+
+const Clock = props =>
+  <div>
+    <style>{`
+      body {
+        margin: 0px;
+        background: ${props.iPhone ? '#ddd' : '#fff'};
+      }
+      svg {
+        transform-origin: 50% ${97 * 2 / 3}%;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        margin-left: -${300 / 2}px;
+        margin-top: -${300 / (1 + 2 / 3)}px;
+        width: ${300}px;
+        height: ${260}px;
+      }
+      @media (min-width: 768px) {
+        svg {
+          margin-left: -${300 * 2 / 2}px;
+          margin-top: -${300 * 2 / (1 + 2 / 3)}px;
+          width: ${300 * 2}px;
+          height: ${260 * 2}px;
+        }
+      }
+      svg.hour {
+        transform: rotate(${props.hourRotation}deg);
+      }
+      svg.minute {
+        transform: rotate(${props.minuteRotation}deg);
+        ${!props.iPhone && `
+        filter: drop-shadow( 0px 0px 12px #888 );
+        -webkit-filter: drop-shadow( 0px 0px 12px #888 );
+        `}
+      }
+    `}</style>
+    <Hour />
+    <Minute />
+  </div>
+
+const iPhone = typeof window !== 'undefined' && window.navigator.platform === 'iPhone'
 
 export default class extends React.Component {
 
   constructor (props) {
     super(props)
     this.state = {
-      now: new Date()
+      now: new Date('1970-01-01T00:00:00Z')
     }
   }
 
   componentDidMount () {
-    this.setState({
-      now: new Date()
-    })
-    setInterval(() => {
+    this.interval = setInterval(() => {
       this.setState({
         now: new Date()
       })
-    }, 1000 * 60)
+    }, 100)
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.interval)
   }
 
   render () {
     const { now } = this.state
-    const startMinuteRotation = now.getMinutes() * 6
-    const startHourRotation = now.getHours() * 30
-    const smallScale = 3
-    const largeScale = 6
+    const minutes = now.getMinutes() + now.getSeconds() / 60 + now.getMilliseconds() / 1000 / 60
+    const minuteRotation = minutes * 6
+    const hourRotation = (now.getHours() + minutes / 60) * 30
 
     return (
       <div>
-        <style>{`
-        body {
-          margin: 0px;
-          background: #fff;
-        }
-        svg {
-          transform-origin: 50% ${100 * 2 / 3}%;
-          position: absolute;
-          top: 50%;
-          left: 50%;
-          margin-left: -${100 * smallScale / 2}px;
-          margin-top: -${100 * smallScale / (1 + 2 / 3)}px;
-          width: ${100 * smallScale}px;
-          height: ${86.6 * smallScale}px;
-        }
-        @media (min-width: 768px) {
-          svg {
-            margin-left: -${100 * largeScale / 2}px;
-            margin-top: -${100 * largeScale / (1 + 2 / 3)}px;
-            width: ${100 * largeScale}px;
-            height: ${86.6 * largeScale}px;
-          }
-        }
-        svg.hour {
-          animation-name: rotateHours;
-          transform: rotate(${startHourRotation}deg);
-          animation-duration: 60s;
-        }
-        @keyframes rotateHours {
-          100% {
-            transform: rotate(${startHourRotation + 30 / 60}deg);
-          }
-        }
-        svg.minute, svg.blur {
-          animation-name: rotateMinutes;
-          transform: rotate(${startMinuteRotation}deg);
-          animation-duration: 60s;
-          filter: drop-shadow( 0px 0px 12px #888 );
-        }
-        @keyframes rotateMinutes {
-          100% {
-            transform: rotate(${startMinuteRotation + 6}deg);
-          }
-        }
-      `}</style>
-        <Hour />
-        <Minute />
+        <Head>
+          <title>Next Time</title>
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <link rel='apple-touch-icon' href='/static/icon.png' />
+        </Head>
+        <Clock iPhone={iPhone} hourRotation={hourRotation} minuteRotation={minuteRotation} />
       </div>
     )
   }
