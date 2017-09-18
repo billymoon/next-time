7a600bbd18f846d99b59633ef024e08d3412cf5e
Billy Moon <billy@itaccess.org>
2017-09-18 23:21:08 +0100

    update code style

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,49 @@
-import React from 'react'
+import { Component } from 'react'
 import Head from 'next/head'
 
-const Hour = props =>
-  <svg className='hour' version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0, 0, 120, 103.924'>
+const iPhone = process.browser && window.navigator.platform === 'iPhone'
+
+const ClockHand = ({ pathString, ...props }) =>
+  <svg {...props} version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0, 0, 120, 103.924'>
     <g>
-      <path d='M7.125,97.753 L33.563,51.962 L60,6.17 L86.438,51.962 L112.876,97.753 L60,97.753 z' fill='#000000' />
+      <path d={pathString} />
     </g>
   </svg>
 
-const Minute = props =>
-  <svg className='minute' version='1.1' xmlns='http://www.w3.org/2000/svg' viewBox='0, 0, 120, 103.924'>
-    <g>
-      <path d='M60,6.17 L86.438,51.962 L112.876,97.753 L7.125,97.753 L33.563,51.962 L60,6.17 z M56.532,24.039 L17.7,91.299 L95.365,91.299 L56.532,24.039 z' fill='#fff' />
-    </g>
-  </svg>
-
-const Clock = props =>
+const Hour = ({ rotation }) =>
   <div>
     <style>{`
-      body {
-        margin: 0px;
-        background: ${props.iPhone ? '#ddd' : '#fff'};
+      .clock svg.hour {
+        transform: rotate(${rotation}deg);
       }
-      svg {
+      .clock svg.hour path {
+        fill: #000;
+      }
+    `}</style>
+    <ClockHand className='hour' pathString='M7.125,97.753 L33.563,51.962 L60,6.17 L86.438,51.962 L112.876,97.753 L60,97.753 z' />
+  </div>
+
+const Minute = ({ rotation }) =>
+  <div>
+    <style>{`
+     .clock svg.minute {
+        transform: rotate(${rotation}deg);
+        ${!iPhone && `
+        filter: drop-shadow( 0px 0px 12px #888 );
+        -webkit-filter: drop-shadow( 0px 0px 12px #888 );
+        `}
+      }
+      .clock svg.minute path {
+        fill: #fff;
+      }
+    `}</style>
+    <ClockHand className='minute' pathString='M60,6.17 L86.438,51.962 L112.876,97.753 L7.125,97.753 L33.563,51.962 L60,6.17 z M56.532,24.039 L17.7,91.299 L95.365,91.299 L56.532,24.039 z' />
+  </div>
+
+const Clock = ({ hourRotation, minuteRotation }) =>
+  <div className='clock'>
+    <style>{`
+      .clock svg {
         transform-origin: 50% ${97 * 2 / 3}%;
         position: absolute;
         top: 50%;
@@ -33,45 +54,27 @@ const Clock = props =>
         height: ${260}px;
       }
       @media (min-width: 768px) {
-        svg {
+        .clock svg {
           margin-left: -${300 * 2 / 2}px;
           margin-top: -${300 * 2 / (1 + 2 / 3)}px;
           width: ${300 * 2}px;
           height: ${260 * 2}px;
         }
       }
-      svg.hour {
-        transform: rotate(${props.hourRotation}deg);
-      }
-      svg.minute {
-        transform: rotate(${props.minuteRotation}deg);
-        ${!props.iPhone && `
-        filter: drop-shadow( 0px 0px 12px #888 );
-        -webkit-filter: drop-shadow( 0px 0px 12px #888 );
-        `}
-      }
     `}</style>
-    <Hour />
-    <Minute />
+    <Hour rotation={hourRotation} />
+    <Minute rotation={minuteRotation} />
   </div>
 
-const iPhone = typeof window !== 'undefined' && window.navigator.platform === 'iPhone'
+export default class extends Component {
 
-export default class extends React.Component {
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      now: new Date('1970-01-01T00:00:00Z')
-    }
+  state = {
+    now: new Date('1970-01-01T00:00:00Z')
   }
 
   componentDidMount () {
-    this.interval = setInterval(() => {
-      this.setState({
-        now: new Date()
-      })
-    }, 100)
+    this.interval = setInterval(this.updateTime, 1000)
+    this.updateTime()
   }
 
   componentWillUnmount () {
@@ -86,6 +89,12 @@ export default class extends React.Component {
 
     return (
       <div>
+        <style>{`
+          body {
+            margin: 0px;
+            background: ${iPhone ? '#ddd' : '#fff'};
+          }
+        `}</style>
         <Head>
           <title>Next Time</title>
           <meta name='apple-mobile-web-app-capable' content='yes' />
@@ -94,5 +103,11 @@ export default class extends React.Component {
         <Clock iPhone={iPhone} hourRotation={hourRotation} minuteRotation={minuteRotation} />
       </div>
     )
+  }
+
+  updateTime = () => {
+    this.setState({
+      now: new Date()
+    })
   }
 }
